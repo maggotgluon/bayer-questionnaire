155dd9d588336574860b16c606c05a09f27076af
Ruttaphong Worachartudomphong <ruttaphong.w@vananava.com>
2024-01-06 08:35:26 +0700

    update click count

--- app/Http/Livewire/Admin/Sidebar.php
+++ app/Http/Livewire/Admin/Sidebar.php
@@ -22,7 +22,7 @@ class Sidebar extends Component
             "Expires"             => "0"
         );
 
-        $columns = array('id', 'age', 'type', 'result', 'score','answer','answer-1','answer-2','answer-3','answer-4', 'create at');
+        $columns = array('id', 'age', 'type', 'result', 'score','answer','answer-1','answer-2','answer-3','answer-4', 'create at','click');
 
         $callback = function() use($qa, $columns) {
             $file = fopen('php://output', 'w');
@@ -65,6 +65,10 @@ class Sidebar extends Component
                 $row['answer-2']  = isset($q->answers['section_part']['answer-2'])?implode(',',$q->answers['section_part']['answer-2']):"-";
                 $row['answer-3']  = isset($q->answers['section_part']['answer-3'])?implode(',',$q->answers['section_part']['answer-3']):"-";
                 $row['answer-4']  = isset($q->answers['section_part']['answer-4'])?implode(',',$q->answers['section_part']['answer-4']):"-";
+                
+
+                $row['click'] = isset($qa->answers['click_boots'])?1:0;
+
                 $row['created_at']  = $q->created_at??"-";
                 // dd($q,$row);
                 
